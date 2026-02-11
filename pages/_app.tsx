502f6e508554405636f55a121ba3fa406fb1a3d9
divij-hub <divijdevkota99@gmail.com>
2026-02-10 19:04:11 -0500

    Changes naming and typos in the folders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // Add this line!
+import '../styles/global.css'; // Add this line!
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider>
       <Component {...pageProps} />
     </ClerkProvider>
   );
