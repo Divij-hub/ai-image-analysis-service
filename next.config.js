ae13fadd0ae6cf11b10b7d2d5e2b5488531f1f3e
divij-hub <divijdevkota99@gmail.com>
2026-02-12 14:12:58 -0500

    added and changed some rules in versel.json file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  rewrites: async () => {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*', // Change 8000 if your Python uses another port
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://127.0.0.1:8000/api/:path*' 
+          : '/api/:path*', 
       },
     ]
   },
